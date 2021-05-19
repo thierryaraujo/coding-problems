4b34c3c12cbcf6ef82a9e0368d2477ff82b72a38
thierry.araujo <thierry.araujo@inss.gov.br>
2021-05-19 16:04:42 -0300

    StringCompression and RotateMatrix

---
+++ algorithms/src/RotateMatrix.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class RotateMatrix {
+
+    public void rotateMatrix(int[][] input){
+        
+    }
+
+    public static void main(String[] args) {
+        RotateMatrix rm = new RotateMatrix();
+        int[][] matrix = {{1,2,3,4},
+                          {5,6,7,8},
+                          {9,10,11,12},
+                          {13,14,15,16}};
+
+        rm.rotateMatrix(matrix);
+        
+    }
+}
--- algorithms/src/StringCompression.java
+++ algorithms/src/StringCompression.java
@@ -1,26 +1,51 @@
 public class StringCompression {
 
     public String compress(String input) {
-        StringBuffer sb = new StringBuffer();
+        int compressedSize = calculateCompression(input);
+        if(compressedSize > input.length()) return input;
+
+        StringBuffer compressed = new StringBuffer(compressedSize);
         int countLetter  = 1;
+       
+        for(int i = 0; i < input.length(); i++) {
+            if (i>0) {
+                if(input.charAt(i) == input.charAt(i-1)) {
+                    countLetter++;
+                } else {
+                    compressed.append(input.charAt(i-1));
+                    compressed.append(countLetter);
+                    countLetter=1;
+                }
+            }
+            if(i==input.length()-1) {
+                compressed.append(input.charAt(i));
+                if(countLetter > 1) compressed.append(countLetter);
+            }
+
+        }
+        return  compressed.toString();
+    }
+
+    public int calculateCompression(String input) {
+        int countLetter  = 1;
+        int countCompressed = 0;
         
         for(int i = 0; i < input.length(); i++) {
             if (i>0) {
                 if(input.charAt(i) == input.charAt(i-1)) {
                     countLetter++;
                 } else {
-                    sb.append(input.charAt(i-1));
-                    if(countLetter > 1) sb.append(countLetter);
+                    countCompressed += 1 + String.valueOf(countLetter).length();
                     countLetter=1;
                 }
             }
             if(i==input.length()-1) {
-                sb.append(input.charAt(i));
-                if(countLetter > 1) sb.append(countLetter);
+                countCompressed += 1 + String.valueOf(countLetter).length();
             }
 
         }
-        return sb.toString();
+
+        return countCompressed;
     }
     public static void main(String[] args) {
         StringCompression stringCompression = new StringCompression();
@@ -29,5 +54,6 @@ public class StringCompression {
         System.out.println(stringCompression.compress("aabbcc"));
         System.out.println(stringCompression.compress("ttttt"));
         System.out.println(stringCompression.compress("m"));
+        System.out.println(stringCompression.compress("aaaAAAbBBB"));
     }
 }
